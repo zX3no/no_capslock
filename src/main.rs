9273daa0d1ecaffd7800757c3d73504f35ce735e
zX3no <dr_xeno@hotmail.com.au>
2022-07-16 15:17:17 +0930

    fixed missing key up

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,16 @@ use std::ptr::null_mut;
 use winapi::{
     shared::windef::HHOOK,
     um::winuser::{
-        keybd_event, CallNextHookEx, DispatchMessageA, GetMessageA, SetWindowsHookExA,
-        TranslateMessage, UnhookWindowsHookEx, KBDLLHOOKSTRUCT, VK_CAPITAL, VK_ESCAPE,
-        WH_KEYBOARD_LL, WM_KEYDOWN, WM_KEYUP,
+        keybd_event, CallNextHookEx, GetMessageA, SetWindowsHookExA, UnhookWindowsHookEx,
+        KBDLLHOOKSTRUCT, KEYEVENTF_KEYUP, VK_CAPITAL, VK_ESCAPE, WH_KEYBOARD_LL, WM_KEYDOWN,
+        WM_KEYUP,
     },
 };
 
 fn main() {
     unsafe {
         let hook_id = SetWindowsHookExA(WH_KEYBOARD_LL, Some(hook_callback), null_mut(), 0);
-        let msg = null_mut();
-
-        while GetMessageA(msg, null_mut(), 0, 0) == 0 {
-            TranslateMessage(msg);
-            DispatchMessageA(msg);
-        }
+        while GetMessageA(null_mut(), null_mut(), 0, 0) == 0 {}
         UnhookWindowsHookEx(hook_id);
     }
 }
@@ -26,8 +21,8 @@ unsafe extern "system" fn hook_callback(code: i32, w_param: usize, l_param: isiz
     let info = *(l_param as *mut KBDLLHOOKSTRUCT);
     if info.vkCode == VK_CAPITAL as u32 {
         match w_param as u32 {
-            WM_KEYUP => keybd_event(VK_ESCAPE as u8, 0x1, 0, 0),
             WM_KEYDOWN => keybd_event(VK_ESCAPE as u8, 0x1, 0, 0),
+            WM_KEYUP => keybd_event(VK_ESCAPE as u8, 0x1, KEYEVENTF_KEYUP, 0),
             _ => (),
         };
         return 1;
